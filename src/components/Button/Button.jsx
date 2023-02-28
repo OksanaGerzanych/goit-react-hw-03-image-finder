f5fb24c7495389a28aa2fc406779495dac2a392a
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-02-28 18:10:55 +0200

    add file

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
+// import PropTypes from 'prop-types';
 
 
 
-export const Button = () => ();
+// export const Button = () => ();
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Gallery } from './ImageGallery.styled';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 
 export const ImageGallery = () => {
