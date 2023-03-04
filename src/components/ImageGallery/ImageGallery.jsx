781f87f63d96bb95ba81f47dacaadbfbae8b52b6
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-03-04 21:28:21 +0200

    add files

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { fetchImage } from 'services/Api'; 
+import { fetchImage } from 'services/api'; 
 import React, { Component } from 'react';
 import { Gallery } from './ImageGallery.styled';
 
